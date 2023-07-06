4a12071d4d20bec0c9bf9f155f144b560443f8c9
ngminhthanh12a3 <ngminhthanh12a3@gmail.com>
2023-07-06 23:38:12 +0000

    0.0.0-Backup

--- api_server/controller/ota/otaSTMV2_CTRL.js
+++ api_server/controller/ota/otaSTMV2_CTRL.js
@@ -6,6 +6,5 @@
  */
 module.exports = async (req, res, next) => {
   const { FILE_PATH } = req.query;
-//   const FILE_BUFFER = await fs.readFileSync(FILE_PATH, { encoding: "binary" });
   res.download(FILE_PATH)
 };
